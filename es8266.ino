4869ed502383cf41a7850458933e1679db3b2a28
Damian Nowak <me@dnowak.dev>
2019-08-15 22:38:08 +0200

    finish arduino

--- es8266.ino
+++ es8266.ino
@@ -1,19 +1,22 @@
-#include <Wire.h>
-#include <DHT.h>
-#include <Adafruit_MPL115A2/Adafruit_MPL115A2.h>
-#include <LightDependentResistor.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-
 //Contains
 const String WIFI_SSID = "WLAN-DYG8HA";
 const String WIFI_PASS = "D80nm8A0Mgg7G8F";
 const int NUMBER_OF_MEASUREMENTS = 10;
-const int TIME_BETWEEN_MEASUREMENTS = 6000; //1 minute
-const String API_URL = "https://weather-station-api.herokuapp.comn /v1/measurments";
+const int TIME_BETWEEN_MEASUREMENTS = 60000; //1 minute
+const String API_URL = "https://weather-station-api.herokuapp.com/proxy";
+const String API_ENDPOINT = "/v1/measurements";
 const String API_FINGERPINT = "08 3B 71 72 02 43 6E CA ED 42 86 93 BA 7E DF 81 C4 BC 62 30"; //https://www.grc.com/fingerprints.htm
-const String API_TOKEN = "1aVtaMl1gQ7qYg5Wa5htY1V2TVk8s9ZHLVykd8e9cSfUKQ50jSTNL2h5M7dX9V8VyyIPfmcQUVhecvXz";
+const String API_TOKEN = "1aVtaMl1gQ7qYg5Wa5htY1V2TVk8s9ZHLVykd8e9cSfUKQ50jSTNL2h5M7dX9V8VyyIPfmcQUVhecvXa";
 const int API_STATION_ID = 1;
+//STOP EDITING HERE!
+
+//Libraries
+#include <Wire.h>
+#include <DHT.h>
+#include <Adafruit_MPL115A2.h>
+#include <LightDependentResistor.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 //Initialize Temperature and Humidity sensor on D3 pin
 uint8_t DHTPin = D3;
@@ -45,7 +48,6 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.println('Waiting for WIFI connection...');
   }
 }
 
@@ -54,30 +56,23 @@ void setup()
  */
 void loop()
 {
-  float temperature[NUMBER_OF_MEASUREMENTS];
-  float humidity[NUMBER_OF_MEASUREMENTS];
-  float pressure[NUMBER_OF_MEASUREMENTS];
-  float illuminance[NUMBER_OF_MEASUREMENTS];
+  float temperatureSum, humiditySum, pressureSum, illuminanceSum = 0.0F;
 
   for (int i = 0; i < NUMBER_OF_MEASUREMENTS; i++)
   {
-    temperature[i] = getTemperature();
-    humidity[i] = getHumidity();
-    pressure[i] = getPressure();
-    illuminance[i] = getIlluminance();
-
-    Serial.println(temperature[i]);
-    Serial.println(humidity[i]);
-    Serial.println(pressure[i]);
-    Serial.println(illuminance[i]);
-    Serial.println();
+    temperatureSum += getTemperature();
+    humiditySum += getHumidity();
+    pressureSum += getPressure();
+    illuminanceSum += getIlluminance();
 
     delay(TIME_BETWEEN_MEASUREMENTS);
   }
 
-  sendToAPI(0, 0, 0, 0);
-
-  delay(5000);
+  sendToAPI(
+      temperatureSum / NUMBER_OF_MEASUREMENTS,
+      humiditySum / NUMBER_OF_MEASUREMENTS,
+      pressureSum / NUMBER_OF_MEASUREMENTS,
+      illuminanceSum / NUMBER_OF_MEASUREMENTS);
 }
 
 /**
@@ -157,14 +152,6 @@ float getIlluminance()
   return i;
 }
 
-/**
-  * Send data to API.
-  * 
-  * @param float temperature
-  * @param float humidity
-  * @param float pressure
-  * @param float illuminance
-  */
 void sendToAPI(
     float temperature,
     float humidity,
@@ -177,23 +164,12 @@ void sendToAPI(
 
     http.begin(API_URL, API_FINGERPINT);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.addHeader("X-Token-Auth", API_TOKEN);
 
-    String params = "token=" + API_TOKEN + "&station_id=" + API_STATION_ID + "&temperature=" + temperature + "&humidity=" + humidity + "&pressure=" + pressure + "&illuminance=" + illuminance;
+    String params = "endpoint=" + API_ENDPOINT + "&station_id=" + API_STATION_ID + "&temperature=" + temperature + "&humidity=" + humidity + "&pressure=" + pressure + "&illuminance=" + illuminance;
 
     int httpCode = http.POST(params);
     String payload = http.getString();
-
-    if (httpCode > 0)
-    {
-      Serial.println(httpCode);
-      Serial.println(payload);
-    }
-    else
-    {
-      Serial.println(http.errorToString(httpCode).c_str());
-    }
-
-    Serial.println();
 
     http.end();
   }
